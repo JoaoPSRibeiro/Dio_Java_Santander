bb5fb9e9ade6355ffa8f5c13401f6f402c3e316d
João Paulo Santos <jpsribeiro@gmail.com>
2024-06-29 18:19:26 -0300

    Removendo comentários

--- Desafios/Dio_Banco_Final/Dio_Banco/src/Main.java
+++ Desafios/Dio_Banco_Final/Dio_Banco/src/Main.java
@@ -7,16 +7,6 @@ public static void main (String[] args) {
 		Cliente JoaoPaulo = new Cliente();
 		JoaoPaulo.setNome("Joao Paulo");
 
-		// Conta cc = new ContaCorrente(JoaoPaulo);
-		// cc.depositar(100);
-		// cc.sacar(0);
-
-		// Conta poupanca = new ContaPoupanca(JoaoPaulo);
-		// poupanca.depositar(0);
-		// cc.transferir(0, poupanca);
-		
-		// cc.imprimirExtrato();
-		// poupanca.imprimirExtrato();
 		Conta poupanca = new ContaPoupanca(JoaoPaulo);
 		Conta cc = new ContaCorrente(JoaoPaulo);
 
