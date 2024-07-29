8fe3c1533b567dfbe5a59d9a8edc794db6531675
João Paulo Santos <jpsribeiro@gmail.com>
2024-07-29 20:50:47 -0300

    atualização final!!!!

--- Desafios/Desafio_Final/santander-2024/src/main/java/me/dio/Santander2024Application.java
+++ Desafios/Desafio_Final/santander-2024/src/main/java/me/dio/Santander2024Application.java
@@ -1,8 +1,13 @@
 package me.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static com.sun.beans.introspect.PropertyInfo.Name.description;
+
+@OpenAPIDefinition(servers = { (@Server(url = "/"), description = "Default Server URL")})
 @SpringBootApplication
 public class Santander2024Application {
 
